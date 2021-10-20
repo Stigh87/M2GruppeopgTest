0d3408ec6d7fc5b50eca897dc92ef650b09af5d8
Stigh87 <88627426+Stigh87@users.noreply.github.com>
2021-10-20 13:59:16 +0200

    Add files via upload

--- view.js
+++ view.js
@@ -4,37 +4,7 @@ function show() {
 let appDiv = document.getElementById('app');
 let html = '';
 
-html += `MAL
-    <button>Start ny meningsmåling</button><br>
-    <div id="activePolls">
-        <h3>Liste over aktive meningsmålinger:</h3><hr><br>
-        <ol>
-            <li>Tittel</li>
-            <li>Tittele</li>
-            <li>Tittelei</li>
-            <li>Titte</li>
-            <li>Tittelo</li>
-            <li></li>
-            <li></li>
-        </ol>
-    </div>
-    
-    <div id="finishedPolls">
-        <h3>Liste over ferdige meningsmålinger:</h3><hr><br>
-        <ol>
-            <li>Ferdig tittel</li>
-            <li>Ferdi tittel</li>
-            <li>Fredig tittel</li>
-            <li>Frodig tittel</li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ol>
-    </div>
-    
-    `
+html += `MAL`
 
 appDiv.innerHTML = html;
 }
-
-//<button id="stopPoll">Avslutt alle</button>
