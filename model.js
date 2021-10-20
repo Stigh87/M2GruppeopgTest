03682c458fc18d8d4d696ff4626a0535d1f7b750
Stigh87 <88627426+Stigh87@users.noreply.github.com>
2021-10-20 14:03:20 +0200

    Add files via upload

--- model.js
+++ model.js
@@ -1,1 +1,64 @@
 //model
+
+const model = {
+ // tilstand app
+  app: {
+      currentPage: 'overview',
+      // 'logIn', 'overview', 'addQuestion', 'results', 'linkPage', 'poll', 'finishPage'
+      currentUser: 'admin',
+      // 'admin', 'guest'(anonym), 'bruker'
+  },
+
+//inputfelter til hver side
+    inputs: {
+        logIn: {
+            username: '',
+            password: '',
+        },
+  
+        questionInput: [
+            {
+                title: "", 
+                questions: [
+                    {
+                        question: "", 
+                        answers: [],
+                        isChecked: false,
+                    },
+                ],
+            },
+        ],
+
+        poll: {
+            questions: [
+                {
+                    question: {
+                        radioInput: false,
+                        textInput: '',
+                    },
+                },
+            ],
+        },
+    },
+
+//data
+    users: [
+        {name: 'admin', pwd: '123', access: 1,},
+        {name: 'guest', pwd: '', access: 2,},
+        {name: 'user1', pwd: '123', access: 2,},
+    ],
+
+    polls: [
+        { id: 1, 
+            tittel: '', 
+            spm: [
+                    {spørsmålstekst: '', 
+                    svarAlternativer: ['', '', '',]
+                    }
+            ],
+        },
+    ],
+
+};
+
+
--- view.js
+++ view.js
@@ -1,10 +1,41 @@
+const appDiv = document.getElementById('app');
+
 //view
 show();
 function show() {
-let appDiv = document.getElementById('app');
 let html = '';
-
-html += `MAL`
+let test = model.Meningsmålinger[0].Tittel;
+html += `MAL
+    <button>Start ny meningsmåling</button><br>
+    <div id="activePolls">
+        <h3>Liste over aktive meningsmålinger:</h3><hr><br>
+        <ol>
+            <li>Tittel</li>
+            <li>Tittele</li>
+            <li>Tittelei</li>
+            <li>Titte</li>
+            <li>Tittelo</li>
+            <li></li>
+            <li></li>
+        </ol>
+    </div>
+    
+    <div id="finishedPolls">
+        <h3>Liste over ferdige meningsmålinger:</h3><hr><br>
+        <ol>
+            <li>Ferdig tittel</li>
+            <li>Ferdi tittel</li>
+            <li>Fredig tittel</li>
+            <li>Frodig tittel</li>
+            <li>${test}</li>
+            <li>${model.Meningsmålinger[0].spm[0].Tekst}</li>
+            <li></li>
+        </ol>
+    </div>
+    
+    `;
 
 appDiv.innerHTML = html;
 }
+
+//<button id="stopPoll">Avslutt alle</button>
